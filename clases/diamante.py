834f754aa1d41a6ed94f771c8078885a468a0bae
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-21 15:34:52 +0000

    retocamos a partir de tus apuntes con nuestros conocimientos

--- clases/diamante.py
+++ clases/diamante.py
@@ -1,18 +1,22 @@
 class A:
     def __init__(self, a):
         self.a = a
+
 class B(A):
     def __init__(self, a, b):
-        A.__init__(self, a)
+        self.a = a
         self.b = b
+
 class C(A):
     def __init__(self, a, c):
-        A.__init__(self, a)
+        self.a = a
         self.c = c
+
 class D(B, C):
     def __init__(self, a, b, c):
-        B.__init__(self, a, b)
-        C.__init__(self, a, c)
+        self.a = a
+        self.b = b
+        self.c = c
+
 d = D(1, 2, 3)
-print(isinstance(d, A), isinstance(d, B), isinstance(d, C))
 print(d.a, d.b, d.c)
