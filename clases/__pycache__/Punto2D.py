18490807597946370e2f8671777f12138bde37e3
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-21 12:48:25 +0000

    definimos la primera función

--- clases/__pycache__/Punto2D.py
+++ clases/__pycache__/Punto2D.py
@@ -4,6 +4,9 @@ class Punto2D:
     self.a2 =a2
     self.b1 = b1
     self.b2 = b2 
- 
+  def punto2d(a1,a2,b1,b2):
+    print("A = X: {}".format(a1), " ; Y: {}".format(a2))
+    print("B = X: {}".format(b1), " ; Y: {}".format(b2))
+  print(punto2d) 
     
   
