ef898e75a2640eb533c7eca5972a77b00736812c
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-21 12:26:04 +0000

    Creamos las variables que definen la orientación de las paredes

--- clases/__pycache__/Herencia_multiple.py
+++ clases/__pycache__/Herencia_multiple.py
@@ -1,3 +1,12 @@
 class paredes:
   def __init__(self,orientacion,ventana):
-    
+    self.orientacion= orientacion 
+    self.ventana=[]
+
+
+
+pared_norte = paredes('NORTE') #lo ponemos en mayúscula porque es una variable constante
+pared_sur = paredes('SUR')
+pared_este = paredes('ESTE')
+pared_oeste = paredes('OESTE')
+  
