5eac0570ba4ff0115461e3141b700fbcd750ec7f
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-21 12:40:13 +0000

    definimos las variables dentro de la función

--- clases/__pycache__/Herencia_multiple.py
+++ clases/__pycache__/Herencia_multiple.py
@@ -1,10 +1,14 @@
 class paredes:
   def __init__(self,orientacion,ventana):
     self.orientacion= orientacion 
-    self.ventana=[]
+    self.ventana= []
 
 class ventana:
   def __init__(self,pared,superficie):
+    self.pared= pared
+    self.superficie= superficie
+    self.pared.ventanas.append(self)
+    
     
 
 
@@ -12,4 +16,3 @@ pared_norte = paredes('NORTE') #lo ponemos en mayúscula porque es una variable 
 pared_sur = paredes('SUR')
 pared_este = paredes('ESTE')
 pared_oeste = paredes('OESTE')
-  
